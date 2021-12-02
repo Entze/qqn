84f55b92da35d04f4efb33f8d47fd8e9a01a1787
jhln <eingang@etagefuenf.com>
2021-12-02 13:52:49 +0100

    fead: expand RSA stop

---
+++ qqn/RSA_POC1.py
@@ -0,0 +1,21 @@
+
+# import own Infer
+
+from webppl import Infer
+
+def world_generation():
+    pass
+
+def utterence_generation():
+    pass
+# factor
+
+def literal_listener():
+    pass
+
+def speaker():
+    pass
+
+def pragmatic_listener():
+    pass
+
