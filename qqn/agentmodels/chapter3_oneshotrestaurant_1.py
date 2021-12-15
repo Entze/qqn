192231997fc85983c559fa12a76b9ddfffd2fd8c
Lukas Grassauer <lukas@grassauer.eu>
2021-12-15 18:12:55 +0100

    feat: add chapter 3 OneShot Restaurant

---
+++ qqn/agentmodels/chapter3_oneshotrestaurant_1.py
@@ -0,0 +1,41 @@
+import torch
+from torch import tensor
+from torch.distributions.utils import logits_to_probs
+
+from qqn.library.action import nr_of_actions_type
+from qqn.library.estimator_messenger import SamplingEstimatingAgentMessenger
+from qqn.library.policy import policy_eff, policy_posterior_eff
+from qqn.library.setvalue_messenger import SetValueMessenger
+from qqn.library.simulate import simulate_by_sampling
+from qqn.library.softmaxagent_messenger import softmax_agent
+from qqn.library.state import StateValueFunctionMessenger
+from qqn.library.transition import TransitionFunctionMessenger
+
+
+def concrete_transition_function(state, action):
+    return torch.stack([tensor(0), action])
+
+
+def concrete_state_value_function(state):
+    return state[1]
+
+
+transition = TransitionFunctionMessenger(concrete_transition_function)
+state_value = StateValueFunctionMessenger(concrete_state_value_function)
+nr_of_actions = SetValueMessenger(nr_of_actions_type, 2)
+
+with nr_of_actions, transition, state_value:
+    print(simulate_by_sampling(tensor([1, 0])))
+    print(policy_eff(tensor([1, 0])))
+
+with nr_of_actions, transition, state_value, softmax_agent():
+    print(simulate_by_sampling(tensor([1, 0])))
+    print(policy_eff(tensor([1, 0])))
+    print(logits_to_probs(policy_posterior_eff(tensor([1, 0])).float()))
+
+with nr_of_actions, state_value, transition, softmax_agent(), SamplingEstimatingAgentMessenger(min_estimation_value=0,
+                                                                                               max_estimation_value=1,
+                                                                                               nr_of_bins=4):
+    print(simulate_by_sampling(tensor([1, 0])))
+    print(policy_eff(tensor([1, 0])))
+    print(logits_to_probs(policy_posterior_eff(tensor([1, 0])).float()))
