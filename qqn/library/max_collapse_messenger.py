9bfa09fb04e924c0b39a67d0deec26e0b24ed699
Lukas Grassauer <lukas@grassauer.eu>
2021-12-17 11:58:56 +0100

    refactor: make softmaxagent similar to other messenger add max collapse messenger

---
+++ qqn/library/max_collapse_messenger.py
@@ -0,0 +1,31 @@
+from numbers import Number
+
+import torch
+from torch import Tensor
+
+from qqn.library.action import action_collapse_type
+from qqn.library.common import snd
+from qqn.library.setvalue_messenger import SetValueMessenger
+
+
+class MaxCollapseMessenger(SetValueMessenger):
+
+    def __init__(self):
+        super().__init__(action_collapse_type, None)
+
+    def _access(self, estimations, *args, **kwargs):
+
+        if isinstance(estimations, list) and len(estimations) > 0 and isinstance(estimations[0], tuple):
+            estimations.sort(key=snd, reverse=True)
+            if isinstance(estimations[0][1], Number):
+                return estimations[0][1]
+            elif isinstance(estimations[0][1], Tensor):
+                estimations = list(map(snd, estimations))
+                estimations = torch.stack(estimations)
+
+        if isinstance(estimations, Tensor):
+            return torch.max(estimations)
+
+        raise NotImplementedError(
+            f"Cannot select from ratings of type {type(estimations).__name__}, "
+            f"you have to use a messenger that processes {str(action_collapse_type)}")
--- qqn/library/softmaxagent_messenger.py
+++ qqn/library/softmaxagent_messenger.py
@@ -2,18 +2,38 @@ from numbers import Number
 
 import pyro
 import pyro.distributions as dist
+import torch
 from pyro.distributions import Distribution
 from torch import Tensor, tensor
 
 from qqn.library.action import action_select_type
-from qqn.library.common import snd
-from qqn.library.effect import Messenger
+from qqn.library.common import snd, fst
+from qqn.library.setvalue_messenger import SetValueMessenger
 
 
-class SoftmaxAgentMessenger(Messenger):
+class SoftmaxAgentMessenger(SetValueMessenger):
 
-    def __init__(self, ):
-        super().__init__()
+    def __init__(self):
+        super().__init__(action_select_type, None)
+
+    def _access(self, ratings, *args, **kwargs):
+
+        if isinstance(ratings, list) and len(ratings) > 0:
+            if isinstance(ratings[0], Number):
+                ratings = torch.as_tensor(ratings)
+            elif isinstance(ratings[0], tuple):
+                ratings.sort(key=fst)
+                estimations_l = list(map(snd, ratings))
+                if isinstance(ratings[0][1], Number):
+                    ratings = torch.as_tensor(estimations_l)
+                elif isinstance(ratings[0][1], Tensor):
+                    ratings = torch.stack(estimations_l)
+
+        if isinstance(ratings, Tensor):
+            return pyro.sample("action_selection", dist.Categorical(logits=ratings))
+
+        raise NotImplementedError(
+            f"Cannot select from ratings of type {type(ratings).__name__}, you have to use a messenger that processes {str(action_select_type)}")
 
     def process_message(self, msg):
         if msg['type'] == action_select_type:
